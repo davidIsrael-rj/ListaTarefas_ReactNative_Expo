ad50d87905bcbd57a5de5d1cd0a61254e0f40b01
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-04 05:10:49 -0300

    Estilizando componentes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,76 @@
-import { Text } from "react-native";
+import { Button, Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import logo from "../assets/images/logo.png"
+import { Alert } from "react-native";
+import { colors } from "../constants/colors";
 
 export default function RootLayout() {
-  return <Text>Olá mundo! ola</Text>
+  return (
+    <ScrollView style={styles.mainContainer}>
+      <View style={styles.rowContainer}>
+        <Image style={styles.image} source={logo} />
+        <Text style={styles.title}>Minhas Tarefas</Text>
+      </View>
+
+      <View style={styles.rowContainer}>
+        <TextInput style={styles.input} />
+        <Pressable
+          onPress={() => Alert.alert("Olá Mundo!!")}
+          style={({pressed}) =>[styles.button, {backgroundColor: pressed ? "blue" :  colors.primary}]}>
+          <Text style={styles.buttonText}>+</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+  )
 }
+
+const styles = StyleSheet.create({
+
+  image: {
+    width: 70,
+    height: 70
+  },
+  title: {
+    fontSize: 30,
+    fontFamily: "Calibri",
+    fontWeight: 600,
+    color: colors.primary
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 16,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 20,
+    flexGrow: 1,
+  },
+  button: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: colors.primary,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20
+  },
+  textContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  mainContainer: {
+    margin: 20
+  },
+  rowContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    marginBottom: 20
+  }
+
+})
