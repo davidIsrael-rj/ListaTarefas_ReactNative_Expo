2e3815ff99f55802a08d415d88e9aa9d61714d86
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-05 15:36:21 -0300

    Renderizando uma lista com FlatList

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,23 @@
-import { Button, Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from "react-native";
 import logo from "../assets/images/logo.png"
 import { Alert } from "react-native";
 import { colors } from "../constants/colors";
 
 export default function RootLayout() {
 
-  const tastks =[
-    {id: 1, completed: true, text: "Fazer café"},
-    {id: 2, completed: false, text: "Estudar React Native"},
-    {id: 3, completed: false, text: "Acadademia"},
+  const tastks = [
+    { id: 1, completed: true, text: "Fazer café" },
+    { id: 2, completed: false, text: "Estudar React Native" },
+    { id: 3, completed: false, text: "Acadademia" },
   ]
   return (
     <ScrollView style={styles.mainContainer}>
@@ -21,13 +30,15 @@ export default function RootLayout() {
         <TextInput style={styles.input} />
         <Pressable
           onPress={() => Alert.alert("Olá Mundo!!")}
-          style={({pressed}) =>[styles.button, {backgroundColor: pressed ? "blue" :  colors.primary}]}>
+          style={({ pressed }) => [styles.button, { backgroundColor: pressed ? "blue" : colors.primary }]}>
           <Text style={styles.buttonText}>+</Text>
         </Pressable>
       </View>
-      <View>
-        {tastks.map(item => <Text key={item.id}>{item.text}</Text>)}
-      </View>
+      <FlatList 
+        data={tastks}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </ScrollView>
   )
 }
